cf2aee4ea91b1305f9cc8f98df20b7e5487595af
RustamAleev2023 <ridermix@yandex.ru>
2023-11-21 20:59:55 +0200

    Task 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    //Task1
+    //Угадай случайное число
+    public static void task1() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        //System.out.println(randomNumber);
+        System.out.println("Угадайте число от 1 до 100");
+        Scanner scanner = new Scanner(System.in);
+        int num = 0;
+
+        int counter = 0;
+
+        while (num != randomNumber) {
+            System.out.println("Введите число");
+            num = scanner.nextInt();
+
+            if (num > randomNumber) {
+                System.out.println("Меньше");
+            }
+            if (num < randomNumber) {
+                System.out.println("Больше");
+            }
+            counter++;
+        }
+
+        System.out.println("Вы отгадали за " + counter + " попыток");
+
+    }
 }
